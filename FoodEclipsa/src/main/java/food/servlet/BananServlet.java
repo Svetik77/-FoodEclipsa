853e9189822221d94e527dbe1a8d2be136dd67e7
Milovanie <zirka7777@gmail.com>
2018-08-05 14:05:43 +0200

    loggers

--- FoodEclipsa/src/main/java/food/servlet/BananServlet.java
+++ FoodEclipsa/src/main/java/food/servlet/BananServlet.java
@@ -1,4 +1,4 @@
-package servlet;
+package food.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,15 +7,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Servlet implementation class BananServlet
  */
 @WebServlet("/Banan")
 public class BananServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger LOG  = LoggerFactory.getLogger(BananServlet.class);
+
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		LOG.debug("forward to banan jsp");
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 		
